8d46223ec14712c4493aff0ec91c2b00de944414
Neel Modi <nmodi6371@gmail.com>
2025-07-12 16:30:07 -0400

    navigation

--- app1/views.py
+++ app1/views.py
@@ -15,3 +15,12 @@ def about(request):
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def shop(request):
+    return render(request, 'shop.html')
